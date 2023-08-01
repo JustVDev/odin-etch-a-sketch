5e8c8dea28f66bb4bd5dacba8f59b05915821dec
Justin V <127573153+JustVDev@users.noreply.github.com>
2023-07-31 20:25:49 -0500

    Finish project, still need to do extra credit

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const grid = document.querySelector('.grid');
 
 //gridsize is how many by how many (i.e 16x16)
-const GRIDSIZE = 16;
 
-//create divs here
-for (let i = 0; i < GRIDSIZE * GRIDSIZE; i++) {
-	const div = document.createElement('div');
-	div.classList.add('square');
-	grid.append(div);
+let gridSize = gridSizeInput.value;
+createGrid(gridSize);
+label.textContent = gridSize;
+
+gridSizeInput.addEventListener('mouseup', () => {
+	gridSize = gridSizeInput.value;
+	grid.innerHTML = '';
+	createGrid(gridSize);
+	label.textContent = gridSize;
+});
+
+function createGrid(gridSize) {
+	//creates squares
+	for (let i = 0; i < gridSize * gridSize; i++) {
+		const div = document.createElement('div');
+		div.classList.add('square');
+		div.style.flex = `1 0 calc((100%) / ${gridSizeInput.value})`;
+		grid.append(div);
+	}
+	const squares = document.querySelectorAll('.square');
+	squares.forEach((square) => square.addEventListener('click', draw));
 }
-
-const squares = document.querySelectorAll('.square');
-
-squares.forEach((square) => square.addEventListener('click', draw));
 
 function draw(e) {
 	e.target.style.background = 'green';
+	// turn off trailing effect
 	e.target.style.transition = 'none';
 }
-
-console.log(squares);
